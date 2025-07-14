422955470a19cb2ea8af5079e1eb3f5666e4094b
A.R.A.H.M.A.N <rahman@ARAHMANs-MacBook-Pro.local>
2025-07-15 07:48:23 +0800

    feat: Initialize Spring Boot project structure

---
+++ src/main/java/io/github/rahman/hotel_booking_api/HotelBookingApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.rahman.hotel_booking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingApiApplication.class, args);
+	}
+
+}
